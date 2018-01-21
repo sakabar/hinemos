5e884c6ae703589a4bd33e60f078e2204c70d2c6
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2018-01-21 21:55:44 +0900

    v0.1.2

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 const init = () => {
-    location.href = URL_ROOT + '/top.html?version=v0.1.1';
+    location.href = URL_ROOT + '/top.html?version=v0.1.2';
 };
 
 init();
--- src/js/signout.js
+++ src/js/signout.js
@@ -1,6 +1,6 @@
 const init = () => {
     localStorage.clear();
-    location.href = URL_ROOT + '/signin.html?version=v0.1.1';
+    location.href = URL_ROOT + '/signin.html?version=v0.1.2';
 };
 
 init();
