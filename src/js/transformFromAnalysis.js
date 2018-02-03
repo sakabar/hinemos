065e7b1fe33b73d0b76890388fda2f17f12b4e66
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2018-02-03 23:41:09 +0900

    TOPページとFAQを更新

--- src/js/transformFromAnalysis.js
+++ src/js/transformFromAnalysis.js
@@ -5,7 +5,7 @@ const showMove = (setup, move1, move2) => {
     if (setup === '') {
         return '[' + move1 + ',' + move2 + ']';
     } else {
-        return setup + ' [' + move1 + ',' + move2 + ']';
+        return '[' + setup + ' [' + move1 + ',' + move2 + ']]';
     }
 };
 
