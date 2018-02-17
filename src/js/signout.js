1b888e7b3e63ac23f83f4d1304a42973baaaac6b
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2018-02-17 13:59:48 +0900

    v0.2.3

--- src/js/signout.js
+++ src/js/signout.js
@@ -1,6 +1,6 @@
 const init = () => {
     localStorage.clear();
-    location.href = URL_ROOT + '/signin.html?version=v0.2.2';
+    location.href = URL_ROOT + '/signin.html?version=v0.2.3';
 };
 
 init();
