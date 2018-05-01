86b4858ff91e74c0c0a5bbde7e32d037f728ee03
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2018-05-01 09:34:33 +0900

    notFoundLettersに関するテストを追加

--- test/registerLetterPair.js
+++ test/registerLetterPair.js
@@ -27,5 +27,24 @@ describe('registerLetterPair.js', () => {
             assert.deepEqual(actual.letterPairs, expected);
             assert.deepEqual(actual.notFoundLetters, []);
         });
+
+        it('正常系: 全ユーザのレターペアには含まれているが、衝突してしたため登録できない場合がある', () => {
+            const allLetterPairs = [
+                { userName: 'user1', letters: 'むつ', word: '陸奥', },
+                { userName: 'user2', letters: 'むつ', word: '陸奥', },
+                { userName: 'me', letters: 'ぬつ', word: '陸奥', },
+            ];
+            const myLetterPairs = [
+                { letters: 'ぬつ', word: '陸奥', },
+            ];
+            const lettersSet = new Set([ 'ぬつ', 'むつ', ]);
+
+            const expected = [
+                { letters: 'ぬつ', words: [ '陸奥', ], },
+            ];
+            const actual = registerLetterPairJS.getAllLetterPairs(allLetterPairs, myLetterPairs, lettersSet);
+            assert.deepEqual(actual.letterPairs, expected);
+            assert.deepEqual(actual.notFoundLetters, [ 'むつ', ]);
+        });
     });
 });
