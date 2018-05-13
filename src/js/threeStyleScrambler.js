23a4b60d83cd6c8dbe1581ec1bebe69fdecda74e
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2018-05-14 00:32:43 +0900

    3-styleをランダムに選ぶところまで

--- src/js/threeStyleScrambler.js
+++ src/js/threeStyleScrambler.js
@@ -1,3 +1,4 @@
+const shuffle = require('shuffle-array');
 const utils = require('./utils');
 
 // sticker1とsticker2のパーツを、若い順に並べる
@@ -6,7 +7,7 @@ const getPartPair = (sticker1, sticker2) => {
     const part1 = Array.from(sticker1).sort().join('');
     const part2 = Array.from(sticker2).sort().join('');
 
-    return [ utils.strMin(part1, part2), utils.strMax(part1, part2) ];
+    return [ utils.strMin(part1, part2), utils.strMax(part1, part2), ];
 };
 
 const classifyWithPartPairs = (threeStyles) => {
@@ -35,6 +36,52 @@ const classifyWithPartPairs = (threeStyles) => {
     return ans;
 };
 
+// threeStyleの中から、以下の条件を満たすようにいくつかランダムに選ぶ
+// 同じパーツが2回以上採用されない
+// 登録済みの3-styleから選ばれる
+// できるだけ多くの3-styleを選ぶ (n個選んだ後、まだ条件を満たすような選び方があるならば、停止せずに次を選ぶ)
+// 必ず停止する
+// 許容できる時間で停止する (曖昧...)
+const pickThreeStyles = (threeStyles) => {
+    if (threeStyles.length === 0) {
+        return [];
+    }
+
+    const threeStyleGroups = classifyWithPartPairs(threeStyles);
+
+    const ans = [];
+    const delim = '-'; // 区切り文字。ステッカーに出てこない文字なら何でもいい
+
+    // ペアを列挙 (`${part1}-${part2}` の形式に変形してからflatten)
+    const pairs = Object.keys(threeStyleGroups).map(part1 => Object.keys(threeStyleGroups[part1]).map(part2 => `${part1}${delim}${part2}`));
+    let flattenedPairs = pairs.reduce((acc, val) => acc.concat(val), []);
+
+    while (flattenedPairs.length > 0) {
+        // キーの中からランダムに1つ選ぶ
+        const [ part1, part2, ] = shuffle.pick(flattenedPairs).split(delim);
+        const pickedThreeStyle = shuffle.pick(threeStyleGroups[part1][part2]);
+        ans.push(pickedThreeStyle);
+
+        // 選んだkeyを削除
+        delete threeStyleGroups[part1];
+        delete threeStyleGroups[part2];
+        // *-part1 か *-part2
+        const ks = Object.keys(threeStyleGroups);
+        for (let i = 0; i < ks.length; i++) {
+            const key1 = ks[i];
+            delete threeStyleGroups[key1][part1];
+            delete threeStyleGroups[key1][part2];
+        }
+
+        const pairs = Object.keys(threeStyleGroups).map(part1 => Object.keys(threeStyleGroups[part1]).map(part2 => `${part1}${delim}${part2}`));
+
+        // 削除後のkeysを反映
+        flattenedPairs = pairs.reduce((acc, val) => acc.concat(val), []);
+    }
+
+    return ans;
+};
+
 const init = () => {
     //
 };
@@ -43,3 +90,4 @@ init();
 
 exports.getPartPair = getPartPair;
 exports.classifyWithPartPairs = classifyWithPartPairs;
+exports.pickThreeStyles = pickThreeStyles;
--- test/threeStyleScrambler.js
+++ test/threeStyleScrambler.js
@@ -1,10 +1,24 @@
 const assert = require('assert');
-const threeStyleScramberJS = require('../src/js/threeStyleScrambler');
+const threeStyleScramblerJS = require('../src/js/threeStyleScrambler');
+
+// 選択した3-styleに、(バッファを除き)同じパーツのステッカーが2回以上使われていないかどうかを判定
+const isValid = (threeStyles) => {
+    const partSet = new Set();
+
+    for (let i = 0; i < threeStyles.length; i++) {
+        const ts = threeStyles[i];
+        const pair = threeStyleScramblerJS.getPartPair(ts.sticker1, ts.sticker2);
+        partSet.add(pair[0]);
+        partSet.add(pair[1]);
+    }
+
+    return partSet.size === 2 * threeStyles.length;
+};
 
 describe('threeStyleScrambler.js', () => {
     describe('getPartPair()', () => {
         it('正常系: 入力が空', () => {
-            const actual = threeStyleScramberJS.getPartPair('DFR', 'UFR');
+            const actual = threeStyleScramblerJS.getPartPair('DFR', 'UFR');
             const expected = [ 'DFR', 'FRU', ];
             assert.deepEqual(actual, expected);
         });
@@ -13,7 +27,7 @@ describe('threeStyleScrambler.js', () => {
     describe('classifyWithPartPairs()', () => {
         it('正常系: 入力が空', () => {
             const arg = [];
-            const actual = threeStyleScramberJS.classifyWithPartPairs(arg);
+            const actual = threeStyleScramblerJS.classifyWithPartPairs(arg);
             const expected = {};
             assert.deepEqual(actual, expected);
         });
@@ -38,7 +52,7 @@ describe('threeStyleScrambler.js', () => {
                 },
             ];
 
-            const actual = threeStyleScramberJS.classifyWithPartPairs(arg);
+            const actual = threeStyleScramblerJS.classifyWithPartPairs(arg);
             const expected = {
                 // 2つのパーツはアルファベットの若い順に並べる
                 // そして、キーに含まれる文字そのものも、アルファベットの若い順に並べる
@@ -94,7 +108,7 @@ describe('threeStyleScrambler.js', () => {
                 },
             ];
 
-            const actual = threeStyleScramberJS.classifyWithPartPairs(arg);
+            const actual = threeStyleScramblerJS.classifyWithPartPairs(arg);
             const expected = {
                 DFR: {
                     FLU: [
@@ -158,7 +172,7 @@ describe('threeStyleScrambler.js', () => {
                 },
             ];
 
-            const actual = threeStyleScramberJS.classifyWithPartPairs(arg);
+            const actual = threeStyleScramblerJS.classifyWithPartPairs(arg);
             const expected = {
                 DFL: {
                     FLU: [
@@ -194,6 +208,78 @@ describe('threeStyleScrambler.js', () => {
                 },
             };
             assert.deepEqual(actual, expected);
+        });
+    });
+
+    describe('pickThreeStyles()', () => {
+        it('正常系: 入力が空', () => {
+            const actual = threeStyleScramblerJS.pickThreeStyles([]);
+            const expected = [];
+            assert.deepEqual(actual, expected);
+        });
+
+        it('正常系: 入力された3-styleのリストの要素数が1の場合、必ずそれが選ばれる', () => {
+            const arg = [
+                {
+                    buffer: 'UBL',
+                    sticker1: 'DFR',
+                    sticker2: 'LFU',
+                    setup: 'D U M M y',
+                    move1: '',
+                    move2: '',
+                },
+            ];
+
+            const actual = threeStyleScramblerJS.pickThreeStyles(arg);
+            const expected = [
+                {
+                    buffer: 'UBL',
+                    sticker1: 'DFR',
+                    sticker2: 'LFU',
+                    setup: 'D U M M y',
+                    move1: '',
+                    move2: '',
+                },
+            ];
+            assert.deepEqual(actual, expected);
+        });
+
+        it('正常系: 入力された3-styleが3つで、それぞれパーツの重複が無い場合、3つ全てが選ばれる。順番は不定', () => {
+            const ts1 = {
+                buffer: 'UBL',
+                sticker1: 'DFR',
+                sticker2: 'LFU',
+                setup: 'D U M M y',
+                move1: '',
+                move2: '',
+            };
+
+            const ts2 = {
+                buffer: 'UBL',
+                sticker1: 'BDL',
+                sticker2: 'BDR',
+                setup: 'D U M M y',
+                move1: '',
+                move2: '',
+            };
+
+            const ts3 = {
+                buffer: 'UBL',
+                sticker1: 'BRU',
+                sticker2: 'DFL',
+                setup: 'D U M M y',
+                move1: '',
+                move2: '',
+            };
+
+            const arg = [ ts1, ts2, ts3, ];
+
+            const actual = threeStyleScramblerJS.pickThreeStyles(arg);
+
+            assert.deepEqual(actual.includes(ts1), true);
+            assert.deepEqual(actual.includes(ts2), true);
+            assert.deepEqual(actual.includes(ts3), true);
+            assert.deepEqual(isValid(actual), true);
         });
     });
 });
