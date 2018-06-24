48d8cf1ccf0d2b8b65176de820b3ecf256cf7f48
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2018-06-24 18:24:04 +0900

    統計情報ページで、リストが空の時は平均の値を0とする

--- src/js/threeStyleQuizCornerStats.js
+++ src/js/threeStyleQuizCornerStats.js
@@ -25,9 +25,10 @@ const init = () => {
             const avgSecs = results.map(x => x.avg_sec);
             const over5Secs = avgSecs.filter(x => x >= threshold);
             const sum = math.sum(avgSecs);
-            const mean = math.mean(avgSecs);
+            const mean = avgSecs.length === 0 ? 0 : math.mean(avgSecs);
             const sumOver5 = math.sum(over5Secs);
-            const meanIn6 = math.mean(avgSecs.filter(x => x < threshold));
+            const avgSecsIn6 = avgSecs.filter(x => x < threshold);
+            const meanIn6 = avgSecsIn6.length === 0 ? 0 : math.mean(avgSecsIn6);
 
             const p1 = document.createElement('p');
             p1.appendChild(document.createTextNode(`所要時間合計: ${sum.toFixed(1)}秒 (${Math.floor(sum / 60)}分${(Math.floor(sum) % 60)}秒)`));
@@ -57,8 +58,8 @@ const init = () => {
             p6.appendChild(document.createTextNode(`${threshold}秒以上かかっている手順を3回ずつ回す練習にかかる時間: ${(sumOver5 * 3).toFixed(1)}秒 (${Math.floor(sumOver5 * 3 / 60)}分${Math.floor(sumOver5 * 3) % 60}秒)`));
             msgArea.appendChild(p6);
         })
-        .catch(() => {
-            alert('エラーが発生しました');
+        .catch((err) => {
+            alert(`エラーが発生しました:${err}`);
         });
 };
 
