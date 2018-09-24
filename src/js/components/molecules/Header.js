5a9efad10dc84e577cf652935eac53fb327f6ab1
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2018-09-24 23:52:08 +0900

    HTML/JS

--- src/js/components/molecules/Header.js
+++ src/js/components/molecules/Header.js
@@ -13,7 +13,7 @@ const Header = () => (
             </ul>
         </div>
 
-        <a href="./mypage.html"><img className="logo__img--rectangle" src="./tw_header.jpg?version=v1.3.3" alt="ロゴ" /></a>
+        <a href="./mypage.html"><img className="logo__img--rectangle" src="./tw_header.jpg?version=v1.3.4" alt="ロゴ" /></a>
     </header>
 );
 
