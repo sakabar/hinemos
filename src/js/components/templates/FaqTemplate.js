9e9a3b718f5c2b1e8054939453a24f7817179502
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2018-10-07 12:38:17 +0900

    Headerコンポーネントをmoleculesからorganismsに移動

--- src/js/components/templates/FaqTemplate.js
+++ src/js/components/templates/FaqTemplate.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Header from '../molecules/Header';
+import Header from '../organisms/Header';
 
 const Paragraph = (props) => (
     <div>
