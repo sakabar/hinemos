46b1605e971d4eb9234548210953789ad37b582f
Takafumi Sakakibara <takafumi.sakakibara.pdx@gmail.com>
2021-09-05 22:46:42 +0900

    記憶練習の回答画面で表示される記憶時間を小数2桁まで表示するようにした

--- src/js/components/organisms/MemoTrainingMbldRecall/index.js
+++ src/js/components/organisms/MemoTrainingMbldRecall/index.js
@@ -20,7 +20,7 @@ const MemoTrainingMbldRecall = ({
             <Button color="primary" value="回答終了" onClick={(e) => sagaFinishRecallPhase()}/>
             <Br/>
 
-            <Txt>記憶時間:{Math.floor((startRecallMiliUnixtime - startMemoMiliUnixtime) / 1000.0)}秒</Txt>
+            <Txt>記憶時間:{((startRecallMiliUnixtime - startMemoMiliUnixtime) / 1000.0).toFixed(2)}秒</Txt>
             <Br/>
 
             {
